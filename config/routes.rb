73895d2c1257e17150985f76f4b4d3e976f84e32
106hi <hiro3428shs96kzsi@gmail.com>
2023-07-17 06:26:51 +0000

    Add create book function

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: %i[index show edit]
-  resources :books, only: %i[index show edit]
+  resources :books, only: %i[index show edit create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
