04ee50f98dd4a9f0766671197b1c9d4fc6fc70a5
106hi <hiro3428shs96kzsi@gmail.com>
2023-08-24 07:28:22 +0000

    Add profile_image to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books
+  has_one_attached :profile_image
+
+  def get_profile_image
+    (profile_image.attached?)? profile_image: 'no_image.jpeg'
+  end
 end
